1493f96d4939bd16428ff73ef9c8798341fba8d7
Micky Dore <mickydcmp@gmail.com>
2019-05-30 17:04:31 +0100

    Updates

---
+++ precache-manifest.ac64f8806824cf2d3e7b6dd2806ea0b3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1b59c138a013ffc469e6ab31c420312",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "7967e8dc5be378ef7c8f",
+    "url": "/Portfolio/static/css/main.dc2b8207.chunk.css"
+  },
+  {
+    "revision": "9634a2804d5597ac62b3",
+    "url": "/Portfolio/static/js/2.548e0644.chunk.js"
+  },
+  {
+    "revision": "7967e8dc5be378ef7c8f",
+    "url": "/Portfolio/static/js/main.fda066ce.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "05fbf38b405583b79010654f2e80648c",
+    "url": "/Portfolio/static/media/about.05fbf38b.jpg"
+  },
+  {
+    "revision": "cefe7186e66c5386cf23f3ffd78375fc",
+    "url": "/Portfolio/static/media/about2.cefe7186.jpg"
+  },
+  {
+    "revision": "88336ea28eaf9881c1958c178bb75892",
+    "url": "/Portfolio/static/media/home2.88336ea2.jpg"
+  },
+  {
+    "revision": "d4fe95f733a4bc1da39048a0bfff7023",
+    "url": "/Portfolio/static/media/projects.d4fe95f7.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/repository-name/precache-manifest.a938ac1a4ac6b06ee0869ff963221924.js"
+  "/Portfolio/precache-manifest.ac64f8806824cf2d3e7b6dd2806ea0b3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/repository-name/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
