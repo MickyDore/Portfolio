53450dc1ac5cae69cc517513e9e61409935cc1eb
Micky Dore <mickydcmp@gmail.com>
2019-05-31 00:04:04 +0100

    Added hyperlinks to social links

--- src/Components/Content/AboutContent.js
+++ src/Components/Content/AboutContent.js
@@ -3,7 +3,11 @@ import '../../Style/accordion.css';
 const iconPath = process.env.PUBLIC_URL + '/assets/';
 
 class AboutContent extends Component {
-
+  
+  handleLinkClick = (url) => {
+    window.open(url);
+  }
+  
   render() {
     return (
       <div className="content-about-container">
@@ -16,7 +20,7 @@ class AboutContent extends Component {
           <div className="content-about-skills-container">
             <div className="item-1"><b>Skills</b> and <b>Technologies</b></div>
             <div className="content-about-skill-item item-2"><span>&#8226;</span> HTML5, CSS3, JavaScript, React, Redux, ES6.</div>
-            <div className="content-about-skill-item item-3"><span>&#8226;</span> Flexbox, CSS-Grid and Bootstrsap.</div>
+            <div className="content-about-skill-item item-3"><span>&#8226;</span> Flexbox, CSS-Grid and Bootstrap.</div>
             <div className="content-about-skill-item item-4"><span>&#8226;</span> Object Oriented Programming using Python and Java. </div>
             <div className="content-about-skill-item item-5"><span>&#8226;</span> Professional version managament experience using Git.</div>
             <div className="content-about-skill-item item-6"><span>&#8226;</span> Creating RESTFUL APIs with Node and Express. </div>
@@ -25,8 +29,8 @@ class AboutContent extends Component {
           <div className="content-about-contact-info">
             <div>If you want to find out more about me, you can read my CV <a href={`${iconPath}cv_portfolio.pdf`} target="_blank">here.</a></div>
             <div className="content-about-socials-container">
-              <div className="content-about-social-item"><img alt="github" src={`${iconPath}images/github.png`} /></div>
-              <div className="content-about-social-item"><img alt="linkedin" src={`${iconPath}images/linkedin.png`} /></div>
+              <div className="content-about-social-item" onClick={() => this.handleLinkClick("https://github.com/MickyDore/")}><img alt="github" src={`${iconPath}images/github.png`} href="https://github.com/MickyDore/" /></div>
+              <div className="content-about-social-item" onClick={() => this.handleLinkClick("https://www.linkedin.com/in/micky-dore/")}><img alt="linkedin" src={`${iconPath}images/linkedin.png`} /></div>
             </div>
           </div>
         </div>
